4c05534f3b3ff401f37fcf7627c43b29abc4b3bb
Kethelyn Carvalho <kethelyncrb@gmail.com>
2024-04-30 23:19:42 -0300

    section contato e formulário

---
+++ js/email.js
@@ -0,0 +1,6 @@
+const form = document.querySelector ("#form");
+const nameInput = document.querySelector ("#name");
+const emailInput = document.querySelector ("#email");
+const messageTextarea = document.querySelector ("#message");
+
+console.log(form, nameInput, emailInput );
